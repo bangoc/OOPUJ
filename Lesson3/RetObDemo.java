aece92d5743aa873b4ac82e01504a3b7659b994f
Nguyen Ba Ngoc <nguyen_bn@hotmail.com>
2019-02-22 15:30:17 +0700

    add one more example of ob passing and return

---
+++ Lesson3/RetObDemo.java
@@ -0,0 +1,24 @@
+class Test {
+  int a = 10;
+  Test(int a) {
+    this.a = a;
+  }
+}
+
+class Data {
+  Test d[] = {new Test(1), new Test(2), new Test(3)};
+  Test getData(int i) {
+    return d[i];
+  }
+}
+
+class RetObDemo {
+  public static void main(String args[]) {
+    Data db = new Data();
+    Test obj = db.getData(1);
+    System.out.println(obj.a);
+    obj.a = 100;
+    Test obj2 = db.getData(1);
+    System.out.println(obj2.a);
+  }
+}
